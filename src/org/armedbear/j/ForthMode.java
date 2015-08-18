f7cc3924b299e5060a269475da5549d04b7b9a6a
Peter Graves <gnooth@gmail.com>
2015-08-17 18:06:47 -0700

    Forth mode indentation

--- src/org/armedbear/j/ForthMode.java
+++ src/org/armedbear/j/ForthMode.java
@@ -28,7 +28,7 @@ public final class ForthMode extends AbstractMode implements Constants, Mode
     private ForthMode()
     {
         super(FORTH_MODE, FORTH_MODE_NAME);
-        setProperty(Property.INDENT_SIZE, 3);
+        setProperty(Property.INDENT_SIZE, 4);
     }
 
     public static ForthMode getMode()
@@ -49,6 +49,7 @@ public final class ForthMode extends AbstractMode implements Constants, Mode
     protected void setKeyMapDefaults(KeyMap km)
     {
         km.mapKey(KeyEvent.VK_TAB, 0, "tab");
+        km.mapKey(KeyEvent.VK_TAB, CTRL_MASK, "insertTab");
         km.mapKey(KeyEvent.VK_ENTER, 0, "newlineAndIndent");
         km.mapKey(KeyEvent.VK_T, CTRL_MASK, "findTag");
         km.mapKey(KeyEvent.VK_PERIOD, ALT_MASK, "findTagAtDot");
@@ -90,16 +91,53 @@ public final class ForthMode extends AbstractMode implements Constants, Mode
         final Line model = findModel(line);
         if (model == null)
             return 0;
-//         if (model.getText().trim().endsWith(":"))
-//             return indentSize;
-        return buffer.getIndentation(model);
+        final String modelTrim =
+            Utilities.detab(model.getText(), buffer.getTabWidth()).trim();
+        final int modelIndent = buffer.getIndentation(model);
+        final int indented = modelIndent + indentSize;
+        if (modelTrim.startsWith(": ") && !modelTrim.endsWith(" ;"))
+            return indented;
+        if (modelTrim.equalsIgnoreCase("begin") || modelTrim.endsWith(" begin"))
+            return indented;
+        if (modelTrim.equalsIgnoreCase("while") || modelTrim.endsWith(" while"))
+            return indented;
+        if (modelTrim.equalsIgnoreCase("if") || modelTrim.endsWith(" if"))
+            return indented;
+        if (modelTrim.equalsIgnoreCase("else") || modelTrim.endsWith(" else"))
+            return indented;
+        if (modelTrim.equalsIgnoreCase("do") || modelTrim.endsWith(" do"))
+            return indented;
+        if (modelTrim.equalsIgnoreCase("?do") || modelTrim.endsWith(" ?do"))
+            return indented;
+        final String trim =
+            Utilities.detab(line.getText(), buffer.getTabWidth()).trim();
+        final int outdented = modelIndent - indentSize;
+        if (trim.equalsIgnoreCase("while") || trim.equalsIgnoreCase("repeat"))
+            return outdented;
+        if (trim.equalsIgnoreCase("then") || trim.equalsIgnoreCase("else"))
+            return outdented;
+        if (trim.startsWith("then ") || trim.startsWith("else "))
+            return outdented;
+        if (trim.equalsIgnoreCase("loop") || trim.equalsIgnoreCase("+loop"))
+            return outdented;
+        if (trim.startsWith("loop ") || trim.startsWith("+loop"))
+            return outdented;
+        if (trim.startsWith(": "))
+            return 0;
+        if (trim.equals(";") || trim.startsWith("; "))
+            return 0;
+        return modelIndent;
     }
 
     private Line findModel(Line line)
     {
         Line model = line.previous();
-        while (model != null && model.isBlank())
-            model = model.previous();
+        while (model != null) {
+            if (model.isBlank() || model.trim().startsWith("\\ "))
+                model = model.previous();
+            else
+                break;
+        }
         return model;
     }
 }
