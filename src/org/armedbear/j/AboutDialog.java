58721ddfca13ac6a0d86f05935b4ef59f42662b9
Peter Graves <gnooth@gmail.com>
2015-09-16 08:24:38 -0700

    updated about box

--- src/org/armedbear/j/AboutDialog.java
+++ src/org/armedbear/j/AboutDialog.java
@@ -1,7 +1,7 @@
 /*
  * AboutDialog.java
  *
- * Copyright (C) 1998-2009 Peter Graves
+ * Copyright (C) 1998-2015 Peter Graves
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -14,8 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.armedbear.j;
@@ -44,7 +43,7 @@ public class AboutDialog extends AbstractDialog
     if (snapshotInformation != null)
       addStaticText(panel, snapshotInformation);
     addStaticText(panel,
-                  "Copyright (C) 1998-2009 Peter Graves (peter@armedbear.org)");
+                  "Copyright (C) 1998-2015 Peter Graves (gnooth@gmail.com)");
     addStaticText(panel,
                   "J is free software; see the source for copying conditions.");
     addStaticText(panel, "There is ABSOLUTELY NO WARRANTY.");
